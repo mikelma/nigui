b81610105172b7a7e8f44d4ce9a21b355cfa6057
mikelma <mikelma@tutanota.com>
2023-09-06 19:50:03 +0200

    Use BarChart for FFT

--- src/wave/plot.rs
+++ src/wave/plot.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{self, Color32, Label, plot::PlotPoint};
+use eframe::egui::{self, Color32, Label, plot::{PlotPoint, BarChart, Bar}};
 use egui::{
     plot::{Legend, Line, Plot, PlotPoints},
     Vec2,
@@ -20,11 +20,12 @@ pub fn plot_waves(ui: &mut egui::Ui) {
     let wave_buffs = WAVE_BUFFS.read().unwrap();
 
     let colors = vec![
-        Color32::RED,
-        Color32::GREEN,
-        Color32::LIGHT_BLUE,
-        Color32::YELLOW,
+        Color32::from_rgb(255, 59, 71),   // tomato
+        Color32::from_rgb(135, 206, 235), // sky blue
+        Color32::from_rgb(0, 255, 127),   // SpringGreen
+        Color32::from_rgb(106, 90, 205),  // SlateBlue
     ];
+    let mut color_idx = 0;
     let mut idx = 0;
     for (fft_buff, (_, wave_buff)) in fft_buffs.iter().zip(wave_buffs.iter()) {
         ui.allocate_ui(space, |ui| {
@@ -40,23 +41,29 @@ pub fn plot_waves(ui: &mut egui::Ui) {
                         .collect::<Vec<f32>>()
                         .as_slice(),
                 ))
-                .color(colors[idx]);
+                .color(colors[color_idx]);
 
                 let num_bins = WAVE_BUFF_LEN as f64 / 2.0;
                 let nyquist = SAMPLING_RATE as f64 / 2.0;
                 let bin_size = nyquist / num_bins;
-                let fft_values: Vec<[f64; 2]> = fft_buff
+                let fft_bars: Vec<Bar> = fft_buff
                     .iter()
                     .enumerate()
-                    .map(|(i, v)| [(i as f64) * bin_size, *v as f64])
-                    .take_while(|v| v[0] <= 60.0)
+                    .map(|(i, v)|{
+                        let freq = (i as f64) * bin_size;
+                        let mag = *v as f64;
+                        Bar::new(freq, mag)
+                            .width(0.095)
+                            .name(format!("{freq} Hz"))
+                         })
+                    .take_while(|v| v.argument <= 60.0)
                     .collect();
-                let fft_line = Line::new(PlotPoints::new(fft_values));
+                let fft_barchart = BarChart::new(fft_bars).color(colors[color_idx]);
 
-                if idx == colors.len() - 1 {
-                    idx = 0;
+                if color_idx == colors.len() - 1 {
+                    color_idx = 0;
                 } else {
-                    idx += 1;
+                    color_idx += 1;
                 }
 
                 columns[0].horizontal_top(|mut ui| {
@@ -65,7 +72,7 @@ pub fn plot_waves(ui: &mut egui::Ui) {
                     });
                     let legend = Legend::default();
 
-                    Plot::new("Raw wave")
+                    Plot::new(format!("Raw wave {idx}"))
                         .allow_drag(false)
                         .allow_zoom(false)
                         // .include_y(1)
@@ -75,14 +82,15 @@ pub fn plot_waves(ui: &mut egui::Ui) {
                         .show(&mut ui, |plot_ui| plot_ui.line(raw_line));
                 });
 
-                Plot::new("FFT")
+                Plot::new(format!("FFT {idx}"))
                     .allow_drag(false)
                     .allow_zoom(false)
                     .show_y(false)
                     // .include_y(1024)
                     // .center_y_axis(true)
-                    .show(&mut columns[1], |plot_ui| plot_ui.line(fft_line));
+                    .show(&mut columns[1], |plot_ui| plot_ui.bar_chart(fft_barchart));
             });
         });
+        idx += 1;
     }
 }
